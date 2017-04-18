b6d326f22034797d6175cf5ef50cd81a11dc6b5a
Divij <divij.mehra.16@gmail.com>
2017-04-19 08:05:23 +1000

    fixed navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'pages#home'
 
   get 'pages/artists'
-
   get 'pages/catalog'
-
   get 'pages/about'
-
   get 'pages/contact'
-
   get 'pages/thoughts'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
