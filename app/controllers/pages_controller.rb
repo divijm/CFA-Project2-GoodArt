44d8dc780e6fd53fa510b377888418b2cfce9828
Divij <divij.mehra.16@gmail.com>
2017-04-19 21:32:09 +1000

    added functionality of rolify, with some bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,22 +18,6 @@ class PagesController < ApplicationController
   end
 
   def mail
-
-    if params[:contact]
-      email =  params[:contact][:email]
-      message = params[:contact][:message]
-
-      if email.present? || message.present?
-        # Tell the UserMailer to send a welcome email after save
-        ContactMailer.send_contact_email(email, message).deliver_now
-        redirect_to '/contact', notice: 'Email sent!'
-      else
-        flash[:warning] = "Please fill out the form"
-        redirect_to '/contact'
-      end
-
-    end
-
   end
 
 end
