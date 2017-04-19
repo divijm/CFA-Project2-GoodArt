a26019dda9f0b7e4d74f9c8703d1d4f8e8ef08d5
Divij <divij.mehra.16@gmail.com>
2017-04-19 19:10:21 +1000

    mailgun functionality added - needs minor bug fixes

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -3,5 +3,22 @@ class ContactController < ApplicationController
   end
 
   def mail
+
+    if params[:contact]
+      email =  params[:contact][:email]
+      message = params[:contact][:message]
+
+      if email.present? || message.present?
+        # Tell the UserMailer to send a welcome email after save
+        ContactMailer.send_contact_email(email, message).deliver_now
+        redirect_to '/contact', notice: 'Email sent!'
+      else
+        flash[:warning] = "Please fill out the form"
+        redirect_to '/contact'
+      end
+
+    end
+
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,9 @@ Rails.application.routes.draw do
   get 'pages/about'
   # get 'pages/contact'
   get 'pages/thoughts'
-  # get 'contact', to: "contact#index"
-  get 'contact', to: "pages#contact"
-  # post 'contact', to: "contact#mail"
-  post 'contact', to: "pages#mail"
+  get 'contact', to: "contact#index"
+  post 'contact', to: "contact#mail"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
