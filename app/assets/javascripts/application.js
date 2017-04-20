c8e5f5e95c8da9c6acf7a8882bf06bbdba1f94ed
Divij <divij.mehra.16@gmail.com>
2017-04-20 12:38:16 +1000

    rateyrate functionality added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,14 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
+// RatyRate uses jQuery for the views and relies on jQuery so it needs to be after jQuery. It is dependant on jQuery.
+//
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
 //= require jquery
+//= require jquery.raty
+//= require ratyrate
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
--- app/models/art.rb
+++ app/models/art.rb
@@ -3,4 +3,7 @@ class Art < ApplicationRecord
 
   has_attached_file :image, styles: {large:"600x600>", medium: "300x300>", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  ratyrate_rateable "rating"
+
 end
