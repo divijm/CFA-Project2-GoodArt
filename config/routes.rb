78d3c52a7d6ae3d088a916a7d809bdde76a2f7b6
Divij <divij.mehra.16@gmail.com>
2017-04-20 12:01:39 +1000

    added facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   resources :arts
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   root 'pages#home'
 
