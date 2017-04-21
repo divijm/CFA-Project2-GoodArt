31358c01929bbb00197cc582686492f04090edc7
Divij <divij.mehra.16@gmail.com>
2017-04-21 17:20:09 +1000

    made some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it "user is valid" do
+    # build user object with factory girl
+    # difference between CREATE and BUILD
     user = build(:user)
+  require 'pry'; binding.pry
     expect(user).to be_valid
   end
 
+  it "can't create user without password" do
+    # build user object with factory girl
+    user = build(:user, :password => nil)
 
+    expect(user).to_not be_valid
+  end
+
+  # it "can't create user without password" do
+  #   # build user object with factory girl
+  #   user = build(:user, :password => nil)
+  #
+  #   expect(user).to_not be_valid
+  # end
 
 
 
