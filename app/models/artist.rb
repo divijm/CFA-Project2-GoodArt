0873a3a99579366ce72c410619178ae454652aef
Divij <divij.mehra.16@gmail.com>
2017-04-25 22:00:16 +1000

    stlying changes

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ApplicationRecord
   belongs_to :user
   has_many :arts
 
-  has_attached_file :profilepic, styles: {large:"600x600>", medium: "300x300>", thumb: "150x150#"}, :default_url => "default_profile.png"
+  has_attached_file :profilepic, styles: {large:"600x600>", medium: "300x300#", thumb: "100x100#"}, :default_url => "logo3.png"
   validates_attachment_content_type :profilepic, content_type: /\Aimage\/.*\z/
 
   geocoded_by :address
