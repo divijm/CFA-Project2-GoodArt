b3e930afea38e2109e6e3e29ad8c370206e18d95
Divij <divij.mehra.16@gmail.com>
2017-04-26 04:27:41 +1000

    styles added for home and about page

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,7 +2,7 @@ class Art < ApplicationRecord
   belongs_to :user
   belongs_to :artist
 
-  has_attached_file :image, styles: {extra_large: "900x900#", large:"600x600#", medium: "200x200#", thumb: "150x150#"}
+  has_attached_file :image, styles: {large:"600x600#", medium: "300x300#", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   ratyrate_rateable "rating"
