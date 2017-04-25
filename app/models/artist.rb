6960f1d614813a148bc48ff129933b71e59556fd
Divij <divij.mehra.16@gmail.com>
2017-04-25 19:20:06 +1000

    photos and links work now

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,14 +6,7 @@ class Artist < ApplicationRecord
   has_attached_file :profilepic, styles: {large:"600x600>", medium: "300x300>", thumb: "150x150#"}, :default_url => "default_profile.png"
   validates_attachment_content_type :profilepic, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :profilepic,
-  :storage => :cloudinary,
-  :path => ':id/:style/:filename'
-
   geocoded_by :address
   after_validation :geocode
-
-  # reverse_geocoded_by :latitude, :longitude
-  # after_validation :reverse_geocode  # auto-fetch address
 
 end
