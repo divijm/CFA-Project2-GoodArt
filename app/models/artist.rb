e462153963225ebc0c090299f3451827fb38fd51
Divij <divij.mehra.16@gmail.com>
2017-05-02 21:07:10 +1000

    made changes to artists model for heroku

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,13 @@ class Artist < ApplicationRecord
   belongs_to :user
   has_many :arts
 
-  has_attached_file :profilepic, styles: {large:"600x600>", medium: "300x300#", thumb: "100x100#"}, :default_url => "logo3.png"
+  has_attached_file :profilepic, styles: {large:"600x600>", medium: "300x300#", thumb: "100x100#"}, :default_url => "logo3.png",
+  storage: :s3,
+  bucket: ENV['S3_BUCKET_NAME'],
+  s3_credentials: {
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
   validates_attachment_content_type :profilepic, content_type: /\Aimage\/.*\z/
 
   geocoded_by :address
