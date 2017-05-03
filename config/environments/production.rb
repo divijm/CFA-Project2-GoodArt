3010d92c42442e8f4b5829869d4e1b2dbcc6fc5f
Divij <divij.mehra.16@gmail.com>
2017-05-03 15:26:30 +1000

    changed paperclip.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
   # config/environments/production.rb
   config.paperclip_defaults = {
   storage: :s3,
+  s3_host_name: 's3-ap-southeast-2.amazonaws.com',
   s3_credentials: {
     bucket: ENV['S3_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,1 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-ap-southeast-2.amazonaws.com'
+# Paperclip::Attachment.default_options[:s3_host_name] = 's3-ap-southeast-2.amazonaws.com'
