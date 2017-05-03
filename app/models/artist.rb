e334c14571770f72e5274921cf43bed360fbfe28
Divij <divij.mehra.16@gmail.com>
2017-05-03 15:44:04 +1000

    fixed localhost artist bug: :wq exit

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,8 @@ class Artist < ApplicationRecord
   belongs_to :user
   has_many :arts
 
-  has_attached_file :profilepic, styles: {large:"600x600>", medium: "300x300#", thumb: "100x100#"}, :default_url => "minion.png",
-  storage: :s3,
-  bucket: ENV['S3_BUCKET_NAME'],
-  s3_credentials: {
-    bucket: ENV['S3_BUCKET_NAME'],
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_KEY']
-    }
+  has_attached_file :profilepic, styles: {large:"600x600>", medium: "300x300#", thumb: "100x100#"}, :default_url => "minion.png"
+
   validates_attachment_content_type :profilepic, content_type: /\Aimage\/.*\z/
 
   geocoded_by :address
