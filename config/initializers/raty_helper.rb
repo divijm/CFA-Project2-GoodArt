3cbd3bf9434a832042140c3ba07e3f5d2f1008cc
Divij <divij.mehra.16@gmail.com>
2017-05-03 14:58:01 +1000

    added precompiler for ratyrate

---
+++ config/initializers/raty_helper.rb
@@ -0,0 +1,6 @@
+# star_path    = options[:star_path]    || ''
+# star_on      = options[:star_on]      || image_path('star-on.png')
+# star_off     = options[:star_off]     || image_path('star-off.png')
+# star_half    = options[:star_half]    || image_path('star-half.png')
+# cancel_on    = options[:cancel_on]    || image_path('cancel-on.png')
+# cancel_off   = options[:cancel_off]   || image_path('cancel-off.png')
